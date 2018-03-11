bb1e7f59f03399a1d9c9639a5065fdf9fb013b11
CL4uD14 <31929720+CL4uD14@users.noreply.github.com>
2018-03-12 00:13:35 +0100

    adding extra functionality

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    require("./submenu.js");
+    require("./slider.js");
+    require("./hide_img_label.js");
+    require("./order.js");
+});
+
